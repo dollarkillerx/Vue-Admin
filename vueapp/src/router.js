cd7e20ef2f65840092f59a19cfd33c355c5b224e
Dollarkillerx <dollarkiller@vip.qq.com>
2019-08-10 22:55:24 +0800

    check form

---
+++ vueapp/src/router.js
@@ -0,0 +1,27 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+
+Vue.use(Router)
+
+export default new Router({
+  mode: 'history',
+  base: process.env.BASE_URL,
+  routes: [
+    {
+      path: '/',
+      redirect: '/index'
+    },{
+      path: '/index',
+      name: 'index',
+      component: () => import('./views/index.vue')
+    },{
+      path: '/register',
+      name: 'register',
+      component: () => import('./views/register.vue')
+    },{
+      path: '*',
+      name: '404',
+      component: () => import('./views/404.vue')
+    }
+  ]
+})
