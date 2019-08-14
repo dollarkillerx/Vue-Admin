356ee085a242f4b1935c9243d3475c092553dd99
Dollarkillerx <dollarkiller@vip.qq.com>
2019-08-14 14:11:47 +0800

    add izs

--- vueapp/src/components/Dialog.vue
+++ vueapp/src/components/Dialog.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="dialog">
         <el-dialog
-                title="添加分类"
+                :title="dialog.title"
                 :visible.sync="dialog.show"
                 :close-on-click-modal="false"
                 :close-on-press-escape="false"
@@ -11,13 +11,38 @@
             <el-form
                 ref="form"
                 :model="formData"
-                :rules="form_urles"
+                :rules="form_rules"
                 label-width="120px"
                 style="margin: 10px;width: auto;"
             >
-                <el-form-item label="名称:">
-                    <el-input v-model="formData.name" placeholder="分类名称"></el-input>
+                <el-form-item prop="name" label="名称:">
+                    <el-input type="name" v-model="formData.name" placeholder="分类名称"></el-input>
                 </el-form-item>
+
+                <el-form-item prop="language" label="语言:">
+                    <el-input type="language" v-model="formData.language" placeholder="语言名称简写 比如西语: es"></el-input>
+                </el-form-item>
+
+                <el-form-item prop="describe" label="注释:">
+                    <el-input type="describe" v-model="formData.describe" placeholder="注释"></el-input>
+                </el-form-item>
+
+                <el-form-item class="text_right">
+                    <el-button @click="dialog.show = fa">
+                        取消
+                    </el-button>
+                    <template v-if="dialog.type != 'edit'">
+                        <el-button type="primary" @click="onSubmit('form')">
+                            提交
+                        </el-button>
+                    </template>
+                    <template v-else>
+                        <el-button type="primary" @click="onEdit('form')">
+                            修改
+                        </el-button>
+                    </template>
+                </el-form-item>
+
             </el-form>
         </div>
 
@@ -30,21 +55,83 @@
         name: "dialog",
         data(){
             return {
-                formData:{
-                    name: '',
-                    language: '',
-                    describe: ''
-                },
+
                 format_type_list:[
                     "名称",
                     "语言",
                     "注释"
-                ]
+                ],
+                form_rules: {
+                    describe:[
+                        {required:true,message:"收支描述不能为空",trigger:blur}
+                    ],
+                    language:[
+                        {required:true,message:"语言不能为空",trigger:blur}
+                    ],
+                    name:[
+                        {required:true,message:"名称不能为空",trigger:blur}
+                    ]
+
+                }
             }
         },
         props: {
-            dialog:Object
-        }
+            dialog:Object,
+            formData:Object
+        },
+        methods:{
+            onSubmit(form) {
+                this.$refs[form].validate(valid => {
+                    if(valid) {
+                        // console.log(this.formData)
+                        this.$axios.post("/api/sort/add",this.formData)
+                            .then(res => {
+                                // 添加成功
+                                this.$message({
+                                    message:'数据添加成功',
+                                    type: 'success'
+                                });
+                                // 隐藏dialog
+                                this.dialog.show = false;
+                                // 告诉父组件添加成功刷新
+                                this.$emit('update');
+                            }).catch(err => {
+                                // 添加失败
+                                this.$message({
+                                    message:'数据添加失败',
+                                    type:'error'
+                                })
+                        })
+                    }
+                })
+            },
+            onEdit(form) {
+                this.$refs[form].validate(valid => {
+                    if(valid) {
+                        // console.log(this.formData)
+                        this.$axios.post("/api/sort/modify",this.formData)
+                            .then(res => {
+                                // 添加成功
+                                this.$message({
+                                    message:'数据添加成功',
+                                    type: 'success'
+                                });
+                                // 隐藏dialog
+                                this.dialog.show = false;
+                                // 告诉父组件添加成功刷新
+                                this.$emit('update');
+                            }).catch(err => {
+                            // 添加失败
+                            this.$message({
+                                message:'数据添加失败',
+                                type:'error'
+                            })
+                        })
+                    }
+                })
+
+            }
+        },
     }
 </script>
 
--- vueapp/src/views/sortList.vue
+++ vueapp/src/views/sortList.vue
@@ -8,83 +8,95 @@
             </el-form>
         </div>
         <div class="table_container">
-        <el-table
-                :data="tableData"
-                max-height="450"
-                border
-                style="width: 100%">
-            <el-table-column
-                    prop="id"
-                    label="Id"
-                    align="center"
-                    width="60">
-            </el-table-column>
-            <el-table-column
-                    prop="name"
-                    label="名称"
-                    align="center"
+            <el-table
+                    :data="tableData"
+                    max-height="450"
+                    border
+                    style="width: 100%">
+                <el-table-column
+                        prop="id"
+                        label="Id"
+                        align="center"
+                        width="60">
+                </el-table-column>
+                <el-table-column
+                        prop="name"
+                        label="名称"
+                        align="center"
 
-            >
-            </el-table-column>
-            <el-table-column
-                    prop="language"
-                    label="语言"
-                    align="center"
+                >
+                </el-table-column>
+                <el-table-column
+                        prop="language"
+                        label="语言"
+                        align="center"
 
-                    width="80">
-            </el-table-column>
-            <el-table-column
-                    prop="describe"
-                    label="备注"
-                    align="center"
+                        width="80">
+                </el-table-column>
+                <el-table-column
+                        prop="describe"
+                        label="备注"
+                        align="center"
 
-            >
-            </el-table-column>
-            <el-table-column
-                    prop="create_time_str"
-                    label="创建时间"
-                    align="center"
-            >
-                <template slot-scope="scope">
-                    <i class="el-icon-time"></i>
-                    <span style="margin-left: 10px">{{ scope.row.create_time_str }}</span>
-                </template>
-            </el-table-column>
-            <el-table-column
-                    label="操作"
-                    prop="operation"
-                    align="center"
-                    fixed="right"
-                    width="160"
-            >
-                <template slot-scope="scope">
-                    <el-button
-                            type="warning"
-                            size="mini"
-                            icon="edit"
-                            @click="handleEdit(scope.$index, scope.row)">编辑</el-button>
-                    <el-button
-                            size="mini"
-                            type="danger"
-                            icon="delete"
-                            @click="handleDelete(scope.$index, scope.row)">删除</el-button>
-                </template>
-            </el-table-column>
-        </el-table>
+                >
+                </el-table-column>
+                <el-table-column
+                        prop="create_time_str"
+                        label="创建时间"
+                        align="center"
+                >
+                    <template slot-scope="scope">
+                        <i class="el-icon-time"></i>
+                        <span style="margin-left: 10px">{{ scope.row.create_time_str }}</span>
+                    </template>
+                </el-table-column>
+                <el-table-column
+                        label="操作"
+                        prop="operation"
+                        align="center"
+                        fixed="right"
+                        width="160"
+                >
+                    <template slot-scope="scope">
+                        <el-button
+                                type="warning"
+                                size="mini"
+                                icon="edit"
+                                @click="handleEdit(scope.$index, scope.row)">编辑
+                        </el-button>
+                        <el-button
+                                size="mini"
+                                type="danger"
+                                icon="delete"
+                                @click="handleDelete(scope.$index, scope.row)">删除
+                        </el-button>
+                    </template>
+                </el-table-column>
+            </el-table>
         </div>
-        <Dialog :dialog="dialog"></Dialog>
+        <Dialog :dialog="dialog" :formData="formData" @update="getProfile"></Dialog>
     </div>
 </template>
 
 <script>
     import Dialog from "../components/Dialog";
+
     export default {
         name: "sortList",
         data() {
             return {
                 tableData: [],
-                dialog:{
-                    show:false
+                formData: {
+                    name: '',
+                    language: '',
+                    describe: '',
+                    id: 0,
+                },
+                dialog: {
+                    show: false,
+                    type: "",
+                    id: "",
+                    title: "添加分类"
                 }
             };
         },
@@ -104,13 +116,62 @@
                         console.log(err)
                     })
             },
-            handleEdit(index,row) {
-                console.log(row.id)
+            handleEdit(index, row) {
+                this.dialog = {
+                    show: true,
+                    title: '修改',
+                    id: row.id,
+                    type: 'edit'
+                };
+                console.log(this.dialog);
+
+                // 获取数据 放入
+
+                // 请求单个完整数据填充
+                this.$axios.get(`/api/sort/data/${row.id}`)
+                    .then(data => {
+                        console.log(data.data);
+                        let datas = data.data;
+                        this.formData = {
+                            name: datas.name,
+                            language: datas.language,
+                            describe: datas.describe,
+                            id: row.id,
+                        };
+                    }).catch(err => {
+                    this.$message({
+                        message: '获取数据失败',
+                        type: 'error'
+                    })
+                });
+
+
             },
-            handleDelete(index,row) {
+            handleDelete(index, row) {
                 console.log(row.id)
+                this.$axios.delete(`/api/sort/del/${row.id}`)
+                    .then(data => {
+                        this.$message({
+                            message:'删除成功',
+                            type: 'success'
+                        });
+                    }).catch(err => {
+
+                    this.$message({
+                        message:'删除失败',
+                        type:'error'
+                    })
+                })
+                this.getProfile()
             },
             handleAdd() {
+                this.formData = {};
+                this.dialog = {
+                    show: false,
+                    type: "",
+                    id: "",
+                    title: "添加分类"
+                };
                 this.dialog.show = true
             }
         },
@@ -127,9 +188,11 @@
         padding: 16px;
         box-sizing: border-box;
     }
+
     .btnRight {
         float: right;
     }
+
     .pagination {
         text-align: right;
         margin-top: 10px;
